e42f75e5396d629a8371d60ea5b8fec3dd4e694e
Ricardo SIGNES <rjbs@cpan.org>
2004-09-21 12:35:48 +0000

    Import of RJBS/Querylet-Output-Excel-OLE-0.10 from CPAN.
    gitpan-cpan-distribution: Querylet-Output-Excel-OLE
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         RJBS/Querylet-Output-Excel-OLE-0.10.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

---
+++ lib/Querylet/Output/Excel/OLE.pm
@@ -0,0 +1,143 @@
+package Querylet::Output::Excel::OLE;
+use base qw(Querylet::Output);
+
+use strict;
+use warnings;
+
+=head1 NAME
+
+Querylet::Output::Excel::OLE - output query results to Excel via OLE
+
+=head1 VERSION
+
+version 0.10
+
+ $Id: OLE.pm,v 1.2 2004/09/21 12:34:09 rjbs Exp $
+
+=cut
+
+our $VERSION = '0.10';
+
+=head1 SYNOPSIS
+
+ use Querylet::Output::Excel::OLE;
+ use Querylet;
+
+ database: bogosity
+
+ query:
+   SELECT detector_id, measurement
+   FROM   bogon_detections
+
+ output format: excel
+
+=head1 DESCRIPTION
+
+Querylet::Output::Excel::OLE provides an output handler for Querylet that will
+create a new instance of Microsoft Excel using Win32::OLE and populate a
+worksheet with the query results.
+
+=head1 METHODS
+
+This module isa Querylet::Output, and implements the interface described by
+that module.
+
+=over 4
+
+=item C<< default_type >>
+
+The Excel::OLE handler is by default registered for the 'excel' type.
+
+=cut
+
+sub default_type { 'excel' }
+
+=item C<< handler >>
+
+The Querylet::Output::Excel::OLE handler returns a coderef that, when called,
+will create an Excel.Application object, then create a workbook, and then
+populate its worksheet with the querylet's results.
+
+=cut
+
+sub handler { \&_to_excel }
+
+sub _to_excel {
+	my $q = shift;
+
+	my $range = [];
+	push @$range, [ @{$q->columns} ];
+
+	foreach my $row (@{$q->results}) {
+		push @$range, [ @$row{@{$range->[0]}} ];
+	}
+
+	my $rows   = @$range;
+	my $column = column_name(scalar @{$range->[0]});
+
+	return sub {
+		require Win32::OLE;
+		require Win32::OLE::Const;
+		Win32::OLE::Const->import('Microsoft Excel');
+
+		my $xl = Win32::OLE->new("Excel.Application");
+		   $xl->{Visible}=1;
+		my $xls = $xl->Workbooks->Add->Worksheets(1);
+		   $xls->Range("A1:$column$rows")->{Value} = $range;
+		   $xls->Range("A1:${column}1")->{Font}->{Bold} = 1;
+	}
+}
+
+=back
+
+=head1 FUNCTIONS
+
+=over 4
+
+=item C<< column_name($column_number) >>
+
+This converts a column number to a column name, Excel style.  In other words:
+
+    1 -> A
+   26 -> Z
+   27 -> AA
+ 2600 -> CUZ
+
+=cut
+
+sub column_name {
+	my ($number) = @_;
+	return unless $number > 0;
+
+	my $name = '';
+	my $zero  = ord('A') - 1;
+	while ($number) {
+		$name = chr($zero + (my $digit = ($number % 26) || 26)) . $name;
+		($number -= $digit) /= 26;
+	}
+	return $name;
+}
+
+=back
+
+=head1 AUTHOR
+
+Ricardo SIGNES, C<< <rjbs@cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-querylet-output-excelapp@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org>.  I will be notified, and then you'll automatically be
+notified of progress on your bug as I make changes.
+
+=head1 COPYRIGHT
+
+Copyright 2004 Ricardo SIGNES, All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1;
